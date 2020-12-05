bf2055ab228e4842f5376dfbac010583891d523c
Marco <marco@mojica.de>
2020-12-05 14:27:47 +0100

    Save

--- src/index.ts
+++ src/index.ts
@@ -1,58 +1,62 @@
 import * as Automerge from 'automerge'
 
-type state = {
-    cards: Automerge.List<card>
+type State = {
+    nodes: Automerge.List<Node>
 }
 
-type card = {
-    title: string
-    done: boolean
+type Node = {
+    name: string
+    position: [number, number]
 }
 
-let localState = Automerge.from<state>({ cards: [] })
+let localState = Automerge.from<State>({ nodes: [] })
 
-let remoteState = Automerge.init<state>()
+let remoteState = Automerge.init<State>()
 remoteState = mergeDocs(remoteState, localState)
 
-function addCard(
-    state: Automerge.FreezeObject<state>,
-    card: card
-): Automerge.FreezeObject<state> {
-    const newState = Automerge.change(state, "addCard", doc => {
-        doc.cards.push(card)
+const localDoc = new Automerge.WatchableDoc(localState)
+
+localDoc.registerHandler(doc => console.dir(doc))
+
+function addNode(
+    state: Automerge.FreezeObject<State>,
+    node: Node
+): Automerge.FreezeObject<State> {
+    const newState = Automerge.change(state, "addNode", doc => {
+        doc.nodes.push(node)
     })
 
     return newState
 }
 
 function getChanges(
-    oldState: Automerge.FreezeObject<state>,
-    newState: Automerge.FreezeObject<state>
+    oldState: Automerge.FreezeObject<State>,
+    newState: Automerge.FreezeObject<State>
 ): Automerge.Change[] {
     return Automerge.getChanges(oldState, newState)
 }
 
 function applyChanges(
-    state: Automerge.FreezeObject<state>,
+    state: Automerge.FreezeObject<State>,
     changes: Automerge.Change[]
-): Automerge.FreezeObject<state> {
+): Automerge.FreezeObject<State> {
     return Automerge.applyChanges(state, changes)
 }
 
 function mergeDocs(
-    local: Automerge.FreezeObject<state>,
-    remo: Automerge.FreezeObject<state>
-): Automerge.FreezeObject<state> {
+    local: Automerge.FreezeObject<State>,
+    remo: Automerge.FreezeObject<State>
+): Automerge.FreezeObject<State> {
     return Automerge.merge(local, remo)
 }
 
-console.log("localstate", localState.cards.length)
-console.log("remoteState", remoteState.cards.length)
+console.log("localstate", localState.nodes.length)
+console.log("remoteState", remoteState.nodes.length)
 
 
 
 // change local state
-let newLocalState = addCard(localState, { title: "card1", done: false })
+let newLocalState = addNode(localState, { name: "node", position: [0, 0] })
 let changes = getChanges(localState, newLocalState)
 
 // apply initial changes to
@@ -61,13 +65,13 @@ let newRemoteState = applyChanges(remoteState, changes)
 localState = newLocalState
 remoteState = newRemoteState
 
-console.log("localstate", newLocalState.cards.length)
-console.log("remoteState", newRemoteState.cards.length)
+console.log("localstate", newLocalState.nodes.length)
+console.log("remoteState", newRemoteState.nodes.length)
 
 
 
 // change local state
-newLocalState = addCard(localState, { title: "card1", done: false })
+newLocalState = addNode(localState, { name: "node0", position: [0, 0] })
 changes = getChanges(localState, newLocalState)
 
 // apply to remoteState
@@ -76,31 +80,29 @@ newRemoteState = applyChanges(remoteState, changes)
 localState = newLocalState
 remoteState = newRemoteState
 
-console.log("localstate", newLocalState.cards.length)
-console.log("remoteState", newRemoteState.cards.length)
+console.log("localstate", newLocalState.nodes.length)
+console.log("remoteState", newRemoteState.nodes.length)
 
 
 
 
 // change local state
-newLocalState = addCard(localState, { title: "card1", done: false })
+newLocalState = addNode(localState, { name: "node00", position: [0, 0] })
 
 for (let i = 0; i < 20; i++) {
-    newLocalState = addCard(newLocalState, { title: "card1", done: false })
+    newLocalState = addNode(newLocalState, { name: "node" + i, position: [0, 0] })
 }
 
 changes = getChanges(localState, newLocalState)
 
-// apply to remoteState
+// apply to remoteStates
 newRemoteState = applyChanges(remoteState, changes)
 
 localState = newLocalState
 remoteState = newRemoteState
 
-console.log("localstate", newLocalState.cards.length)
-console.log("remoteState", newRemoteState.cards.length)
-
-// console.log("savedState", Automerge.save(localState))
+console.log("localstate", newLocalState.nodes.length)
+console.log("remoteState", newRemoteState.nodes.length)
 
 // const history = Automerge.getHistory(localState)
 
